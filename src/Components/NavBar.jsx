e76883d22e4ad684de57526a1fbc160122afeee0
Gaurav <gauravsomani52750@gmail.com>
2022-07-28 00:04:47 +0530

    added navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import {Link} from 'react-router-dom';
+
 const NavBar = () => {
   return (
     <div>
-        
+        <ul id='nav'>
+            <li>
+                <Link to='/' > Home </Link>
+            </li>
+            <li>
+                <Link to='/post/create' > Create </Link>
+            </li>
+        </ul>
     </div>
   )
 }
